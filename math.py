d9aad976b2d31b3405806df5507e17186a6d325f
Abhinav Sawarkar <71547093+abhinavishere@users.noreply.github.com>
2022-01-27 12:36:04 +0530

    Subtract implementation on Feature1

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ def add(x,y):
     pass
 # Subtraction implementation
 def subtract(x,y):
-    pass
+    return x-y # on Feature1 Branch
 # Multiplicaiton implementation
 def multiply(x,y):
     pass
