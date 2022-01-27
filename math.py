9ee870253763169946dd50e024cc77749cb7c56d
Abhinav Sawarkar <71547093+abhinavishere@users.noreply.github.com>
2022-01-27 12:03:28 +0530

    Initial Commit of Math.py

---
+++ math.py
@@ -0,0 +1,16 @@
+# Adding implementation
+def add(x,y):
+    return x+y
+# Subtraction implementation
+def subtract(x,y):
+    return x-y
+# Multiplicaiton implementation
+def multiply(x,y):
+    return x*y
+#Divide Implementation
+def divide(x,y):
+    if y==0:
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
+
